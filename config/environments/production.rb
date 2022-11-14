29d71a17232c7fa2c82b9c61a55e988996a20962
tolaseadegbite <tolasekelvin@gmail.com>
2022-11-14 09:22:15 +0100

    Modify database.yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.hosts << "nbtefpa.up.railway.app"
+  # config.hosts << "nbtefpa.up.railway.app"
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
